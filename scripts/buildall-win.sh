d2001ca23b82741d386108cf622134cd87bb3842
Jack Jansen <Jack.Jansen@cwi.nl>
2021-06-04 10:46:14 +0200

    Copied build scripts over from cwipc_test repo, modified for subrepo setup

---
+++ scripts/buildall-win.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+set -x
+errorexit() {
+	echo '** Error: your buildall-win did not succeed. Check error log above.'
+	exit 1
+}
+trap errorexit ERR
+dirname=`dirname $0`
+dirname=`cd $dirname/..; pwd`
+cd $dirname
+mkdir -p installed
+instdir=`cd installed; pwd`
+instdir=`cygpath -w "$instdir"`
+
+notest=
+case x$1 in
+x--notest)
+	notest="notest"
+	shift
+	;;
+esac
+case x$1 in
+x)
+	all="cwipc_util cwipc_realsense2 cwipc_codec cwipc_kinect"
+	;;
+*)
+	all=$@
+	;;
+esac
+
+for i in $all ; do
+(
+	cd $i
+	(
+		mkdir -p build
+		cd build
+		cmake .. -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX="$instdir" -DJPEG_Turbo_ROOT="C:/libjpeg-turbo64"
+		cmake --build . --config Release
+		if [ "$notest" != "notest" ]; then
+			cmake --build . --config Release --target RUN_TESTS
+		fi
+		cmake --build . --config Release --target INSTALL
+	)	
+)
+done
---
+++ scripts/buildall.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -x
+set -e
+dirname=`dirname $0`
+dirname=`cd $dirname/..; pwd`
+cd $dirname
+notest=
+case x$1 in
+x--notest)
+	notest="notest"
+	shift
+	;;
+esac
+sudo=
+case x$1 in
+x--sudo)
+	sudo="sudo"
+	shift
+	;;
+esac
+case x$1 in
+x)
+	all="cwipc_util cwipc_realsense2 cwipc_codec"
+	if [ `uname` != "Darwin" ]; then
+		all="$all cwipc_kinect"
+	fi
+	;;
+*)
+	all=$@
+	;;
+esac
+	
+for i in $all ; do
+if [ -d $i ]; then
+(
+	echo "** Building $i **"
+	cd $i
+	(
+		mkdir -p build
+		cd build
+		cmake ..
+		make
+		if [ "$notest" != "notest" ]; then
+			make test
+		fi
+		$sudo make install
+	)	
+)
+else
+	echo "** Skipping $i - not found **"
+fi
+done
