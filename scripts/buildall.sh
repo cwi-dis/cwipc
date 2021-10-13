e1e12b83dc80d429881d6016d829474d3be8617e
Jack Jansen <Jack.Jansen@cwi.nl>
2021-10-13 11:39:18 +0200

    If python3 is in PATH use that Python for building

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -4,6 +4,7 @@ set -e
 dirname=`dirname $0`
 dirname=`cd $dirname/..; pwd`
 cd $dirname
+
 notest=
 case x$1 in
 x--notest)
@@ -11,6 +12,7 @@ x--notest)
 	shift
 	;;
 esac
+
 sudo=
 case x$1 in
 x--sudo)
@@ -18,6 +20,7 @@ x--sudo)
 	shift
 	;;
 esac
+
 cmakeargs=
 case x$1 in
 x--cicd)
@@ -25,16 +28,30 @@ x--cicd)
 	shift
 	;;
 esac
+
 # Workaround for brew-installed Qt5 not found by cmake:
 if [ -d /usr/local/opt/qt5/lib/cmake/Qt5 ]; then
 	cmakeargs="$cmakeargs -DQt5_DIR=/usr/local/opt/qt5/lib/cmake/Qt5"
 fi
+# Workaround for finding the wrong Python3 on M1 macs
+pydir=`python3 -c 'import sys ; print(sys.prefix)'`
+case x$pydir in
+x)
+	;;
+x*)
+	cmakeargs="$cmakeargs -DPython3_ROOT_DIR=$pydir"
+	;;
+esac
+
 mkdir -p build
 cd build
 cmake .. $cmakeargs
+
 make
+
 if [ "$notest" != "notest" ]; then
 	make test
 fi
+
 $sudo make install
 
