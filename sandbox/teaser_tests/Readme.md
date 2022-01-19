3292f5b0c6ca3e01ffaf3fbe9d4d9480fd2937cf
Jack Jansen <Jack.Jansen@cwi.nl>
2022-01-19 17:30:47 +0100

    Revived issue40-teaser-alignment branch and added sandbox to test

---
+++ sandbox/teaser_tests/Readme.md
@@ -0,0 +1,5 @@
+To test teaser alignment
+
+ 1) execute download.sh to get the ply and cameraconfig test files
+ 2) run:
+ 	python3 -m cwipc.scripts.cwipc_calibrate --teaser --nograb posterframe-s1_professor_Emc2_r1_t1-33027933.ply --reuse --noinspect
---
+++ sandbox/teaser_tests/download.sh
@@ -0,0 +1,2 @@
+curl --output posterframe-s1_professor_Emc2_r1_t1-33027933.ply  http://192.16.197.145:8086/s1_professor_Emc2/r1_t1/posterframe-s1_professor_Emc2_r1_t1-33027933.ply
+curl --output cameraconfig.xml  http://192.16.197.145:8086/s1_professor_Emc2/r1_t1/cameraconfig.xml
