c429c7e6dfa53568d4cc0463cdfb89bb86a8ee6a
Jack Jansen <Jack.Jansen@cwi.nl>
2022-04-18 22:28:38 +0200

    Allow specifying branch name.

--- sandbox/pullall.sh
+++ sandbox/pullall.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 if [[ "$1" != "-f" && $(git diff --stat) != "" ]]; then
-	echo Repo is dirty:
-	git diff
+	echo Not checking out, repo is dirty:
+	git status
+	git submodule foreach git status
+	echo Error: Not checking out, repo is dirty. Use -f to override.
 	exit
+fi
+if [[ "$1" == "-f" ]]; then
+	shift
+fi
+branch=master
+if [[ "$1" != "" ]]; then
+	branch=$1
+	shift
 fi
 set -x
 git fetch --recurse-submodules
-git checkout master
+git checkout $branch
 git pull --recurse-submodules
-git submodule foreach git checkout master
+git submodule foreach git checkout $branch
 git submodule foreach git pull
 git status
