e7631b364053b8bdf3babf77d5bf1d49c7993ac4
Jack Jansen <Jack.Jansen@cwi.nl>
2022-04-26 15:52:46 +0200

    Extract version numbers from version string

--- sandbox/getgitversion.sh
+++ sandbox/getgitversion.sh
@@ -8,15 +8,37 @@ TAG=$(git describe --abbrev=0 --tags $TAG_COMMIT)
 # Remove a leading v and trailing _stable
 TAG=${TAG/#v/}
 TAG=${TAG/%_stable/}
+# Construct version and version components
+VERSION_COMMIT=""
+VERSION_MAJOR=0
+VERSION_MINOR=0
+VERSION_MICRO=0
+# Start with constructing the version
 if [[ "$TAG" == "" ]] ; then
 	# No tag found, use the current commit only
-	VERSION="0.0.0-${CURRENT_COMMIT}"
+	VERSION="0.0.0+${CURRENT_COMMIT}"
+	VERSION_COMMIT="+${CURRENT_COMMIT}"
 else
 	if [[ "$TAG_COMMIT" == "CURRENT_COMMIT" ]] ; then
 		# Tag found, and we are exactly there
 		VERSION="${TAG}"
 	else
 		VERSION="${TAG}+${CURRENT_COMMIT}"
+		VERSION_COMMIT="+${CURRENT_COMMIT}"
+	fi
+fi
+if [[ $VERSION =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)\. ]]; then
+	VERSION_MAJOR=${BASH_REMATCH[1]}
+	VERSION_MINOR=${BASH_REMATCH[2]}
+	VERSION_MICRO=${BASH_REMATCH[3]}
+else
+	if [[ $VERSION =~ ^([0-9]+)\.([0-9]+)\. ]]; then
+		VERSION_MAJOR=${BASH_REMATCH[1]}
+		VERSION_MINOR=${BASH_REMATCH[2]}
 	fi
 fi
 echo VERSION=${VERSION}
+echo VERSION_MAJOR=${VERSION_MAJOR}
+echo VERSION_MINOR=${VERSION_MINOR}
+echo VERSION_MICRO=${VERSION_MICRO}
+echo VERSION_COMMIT=${VERSION_COMMIT}
