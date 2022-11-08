80a32e1863c8dd4bb9309e60c02bed64dfac1cf7
Jack Jansen <Jack.Jansen@cwi.nl>
2022-11-08 17:10:57 +0100

    Adding Kinect support

---
+++ unity/nl.cwi.dis.cwipc/Runtime/Scripts/KinectPointCloudReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace cwipc
+{
+    using Timestamp = System.Int64;
+    using Timedelta = System.Int64;
+
+    public class KinectPointCloudReader : BasePointCloudReader
+    {
+        [Tooltip("Filename of cameraconfig.xml file")]
+        public string configFileName;
+       
+        override public void _AllocateReader()
+        {
+            
+            reader = cwipc.kinect(configFileName);
+            if (reader != null)
+            {
+#if CWIPC_WITH_LOGGING
+                Debug.Log($"{Name()}: Started.");
+#endif
+            }
+            else
+                throw new System.Exception($"{Name()}: cwipc_kinect could not be created"); // Should not happen, should throw exception
+        }
+    }
+}
