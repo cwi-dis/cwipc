a94236ecfafc9ec59751fa437b5eb3830d93c549
Jack Jansen <Jack.Jansen@cwi.nl>
2023-11-08 14:07:31 +0000

    Print per-contributing-camera-count pointcounts. Output voxelized pointclouds to files for debuging.

--- sandbox/voxelize_curve.py
+++ sandbox/voxelize_curve.py
@@ -8,11 +8,12 @@ def main():
     pc = cwipc.cwipc_read(sys.argv[1], 0)
     originalCount = pc.count()
     originalCellsize = pc.cellsize()
-    print("iteration,cellSize,pointCount")
-    print(f"0,{originalCellsize},{originalCount}")
+    print("iteration,cellSize,pointCount,increaseFactor,perTileCountCount")
+    print(f"0,{originalCellsize},{originalCount},{0},,")
     newCount = -1
     cellSize = 1
     iteration = 1
+    oldCount = 0
     while cellSize > originalCellsize:
         try:
             newPc = cwipc.cwipc_downsample(pc, cellSize)
@@ -20,10 +21,49 @@ def main():
             print(f"cwipc_downsample: Exception {e}", file=sys.stderr)
             break
         newCount = newPc.count()
-        print(f"{iteration},{cellSize},{newCount}")
+        if oldCount == 0:
+            increaseFactor = 0
+        else:
+            increaseFactor = newCount / oldCount
+        oldCount = newCount
+        histogram = tileHistogram(newPc)
+        histCombined = histogramCombine(histogram)
+        print(f"{iteration},{cellSize},{newCount},{increaseFactor},{histCombined}")
+        cwipc.cwipc_write(f"downsample-{iteration}.ply", newPc)
         newPc.free()
         cellSize = cellSize * 0.7071
         iteration += 1
+
+def tileHistogram(pc : cwipc.cwipc_wrapper) -> list[int]:
+    """For a given point cloud, return the histogram how many points are in each tile"""
+    points = pc.get_points()
+    maxTile = 0
+    p : cwipc.cwipc_point
+    for p  in points:
+        if p.tile > maxTile:
+            maxTile = p.tile
+    rv = [0] * (maxTile+1)
+    for p in points:
+        rv[p.tile] += 1
+    return rv
+
+def histogramCombine(counts : list[int]) -> list[int]:
+    maxBitCount = 0
+    for tileMask in range(len(counts)):
+        if counts[tileMask] > 0:
+            bitCount = countBits(tileMask)
+            if bitCount > maxBitCount:
+                maxBitCount = bitCount
+    rv = [0]*(maxBitCount+1)
+    for tileMask in range(len(counts)):
+        if counts[tileMask] > 0:
+            bitCount = countBits(tileMask)
+            rv[bitCount] = rv[bitCount] + counts[tileMask]
+    return rv
+
+def countBits(src : int) -> int:
+    """Return number of bits set in argument"""
+    return bin(src).count('1')
 
 if __name__ == '__main__':
     main()
