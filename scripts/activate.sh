546454757cd9baf8ad115633c034da6420529397
Jack Jansen <Jack.Jansen@cwi.nl>
2023-11-10 21:38:02 +0100

    Adding bash script (to be sourced) to prepare vscode environment for debugging cwipc

---
+++ scripts/activate.sh
@@ -0,0 +1,14 @@
+# Set PATH and DYLD_LIBRARY_PATH to include built executables and dynamic libraries and enable Python venv.
+# use with "source", don't execute.
+myPath=`realpath $0`
+myDir=`dirname $myPath`
+buildDir=`cd $myDir/../build && pwd`
+export PATH="$buildDir/bin:$PATH"
+# Setting DYLD_LIBRARY_PATH doesn't work, but luckily it usually isn't needed
+# export DYLD_LIBRARY_PATH="$buildDir/lib:$DYLD_LIBRARY_PATH"
+source $buildDir/venv/bin/activate
+# Install editable Python packages
+(cd $buildDir/../cwipc_util/python && python -m pip install -e .)
+(cd $buildDir/../cwipc_codec/python && python -m pip install -e .)
+(cd $buildDir/../cwipc_realsense2/python && python -m pip install -e .)
+(cd $buildDir/../cwipc_kinect/python && python -m pip install -e .)
