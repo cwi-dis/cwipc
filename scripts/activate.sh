1359c9f8b7a14d9c6db4a2d1fb3e6b02ded32b94
Jack Jansen <Jack.Jansen@cwi.nl>
2023-11-13 23:21:58 +0100

    It turns out to be difficult to make a script bash-compatible as well as zsh-compatible...

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,11 +1,10 @@
 # Set PATH and DYLD_LIBRARY_PATH to include built executables and dynamic libraries and enable Python venv.
 # use with "source", don't execute.
-if [ "$BASH_SOURCE" == "" ]; then
+if [[ "${BASH_SOURCE}" == "" ]]; then
     myPath=`realpath "${(%):-%N}"`
 else
-    myPath=`realpath "$BASH_SOURCE"`
+    myPath=`realpath "${BASH_SOURCE}"`
 fi
-set
 myDir=`dirname $myPath`
 buildDir=`cd $myDir/../build && pwd`
 export PATH="$buildDir/bin:$PATH"
