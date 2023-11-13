ec8beec504cbe6141ad082c49f09d90cf22d8f1d
Jack Jansen <Jack.Jansen@cwi.nl>
2023-11-13 23:21:58 +0100

    Cherry pick.

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,14 +1,20 @@
 # Set PATH and DYLD_LIBRARY_PATH to include built executables and dynamic libraries and enable Python venv.
 # use with "source", don't execute.
-myPath=`realpath $0`
+if [[ "${BASH_SOURCE}" == "" ]]; then
+    myPath=`realpath "${(%):-%N}"`
+else
+    myPath=`realpath "${BASH_SOURCE}"`
+fi
 myDir=`dirname $myPath`
 buildDir=`cd $myDir/../build && pwd`
 export PATH="$buildDir/bin:$PATH"
 # Setting DYLD_LIBRARY_PATH doesn't work, but luckily it usually isn't needed
-# export DYLD_LIBRARY_PATH="$buildDir/lib:$DYLD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$buildDir/lib:$DYLD_LIBRARY_PATH"
+export CWIPC_LIBRARY_DIR="$buildDir/lib"
 source $buildDir/venv/bin/activate
 # Install editable Python packages
 (cd $buildDir/../cwipc_util/python && python -m pip install -e .)
 (cd $buildDir/../cwipc_codec/python && python -m pip install -e .)
 (cd $buildDir/../cwipc_realsense2/python && python -m pip install -e .)
 (cd $buildDir/../cwipc_kinect/python && python -m pip install -e .)
+
