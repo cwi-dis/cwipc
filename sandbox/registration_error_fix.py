74a04831367d8fdc3184c2756eb6313b12d229c0
Jack Jansen <Jack.Jansen@cwi.nl>
2023-12-08 16:08:44 +0100

    Minor fixes.

--- sandbox/registration_error_fix.py
+++ sandbox/registration_error_fix.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import math
-from typing import Optional, List, Any
+from typing import Optional, List, Any, Tuple
 import numpy as np
 import scipy.spatial
 from matplotlib import pyplot as plt
@@ -23,25 +23,33 @@ def main():
         pc = cwipc.cwipc_read(sys.argv[1], 0)
     else:
         pc = cwipc.cwipc_read_debugdump(sys.argv[1])
-    camnum_to_fix, correspondence = run_analyzer(pc, basefilename, png_filename, " before", True)
+
+    # This number sets a threashold for the best possible alignment.
+    # xxxjack it should be computed from the source point clouds
+    original_capture_precision = 0.001
+
+    camnum_to_fix, correspondence_error = run_analyzer(pc, original_capture_precision, basefilename, png_filename, " before", True)
+    
     
     step = 1
-    while True:
+    while camnum_to_fix != None:
         png_filename = basefilename + f"_histogram_after_step_{step}.png"
-        new_pc = run_fixer(pc, camnum_to_fix, correspondence)
+        new_pc = run_fixer(pc, camnum_to_fix, correspondence_error)
         pc.free()
         pc = new_pc
-        old_camnum_to_fix, old_correspondence = camnum_to_fix, correspondence
+        old_camnum_to_fix, old_correspondence_error = camnum_to_fix, correspondence_error
         
-        camnum_to_fix, correspondence = run_analyzer(pc, basefilename, png_filename, f" after {step}", True)
-        if camnum_to_fix == old_camnum_to_fix and correspondence >= old_correspondence:
-            print(f"No more improvement. Camera {old_camnum_to_fix} was at {old_correspondence}, now camera {camnum_to_fix} is at {correspondence}")
+        camnum_to_fix, correspondence_error = run_analyzer(pc, original_capture_precision, basefilename, png_filename, f" after {step}", True)
+        if camnum_to_fix == old_camnum_to_fix and correspondence_error >= old_correspondence_error:
+            print(f"No more improvement. Camera {old_camnum_to_fix} was at {old_correspondence_error}, now camera {camnum_to_fix} is at {correspondence_error}")
             break
         step += 1
+    if camnum_to_fix == None:
+        print(f"All cameras have correspondence below capture precision of {original_capture_precision}")
     ply_filename = basefilename + "_after.ply"
     cwipc.cwipc_write(ply_filename, pc)
 
-def run_analyzer(pc : cwipc.cwipc_wrapper, basefilename : str, png_filename : str, extlabel : str, plot : bool):
+def run_analyzer(pc : cwipc.cwipc_wrapper, original_capture_precision : float, basefilename : str, png_filename : str, extlabel : str, plot : bool) -> Tuple[Optional[int], float]:
     analyzer = cwipc.registration.analyze.RegistrationAnalyzerOneToAll()
     analyzer.add_tiled_pointcloud(pc)
     analyzer.label = basefilename + extlabel
@@ -51,8 +59,13 @@ def run_analyzer(pc : cwipc.cwipc_wrapper, basefilename : str, png_filename : st
     print(f"Sorted correspondences {extlabel}")
     for camnum, correspondence, weight in results:
         print(f"\tcamnum={camnum}, correspondence={correspondence}, weight={weight}")
-    camnum_to_fix = results[0][0]
+    camnum_to_fix = None
     correspondence = results[0][1]
+    for i in range(len(results)):
+        if results[i][1] > original_capture_precision:
+            camnum_to_fix = results[i][0]
+            correspondence = results[i][1]
+            break
     if plot:
         analyzer.save_plot(png_filename, True)
     return camnum_to_fix, correspondence
