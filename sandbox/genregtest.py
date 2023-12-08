4b09a382b09e8e11aaf9d069c41b6e4d469f2418
Jack Jansen <Jack.Jansen@cwi.nl>
2023-12-08 16:09:15 +0100

    Allow using loot as a source. Changed the way pointclouds are split.

--- sandbox/genregtest.py
+++ sandbox/genregtest.py
@@ -7,15 +7,16 @@ import cwipc.filters.transform
 MyPoint = Tuple[float, float, float]
 MyBoundingBox = Tuple[float, float, float, float, float, float]
 
-BBOX_0 = (-10, -0.1, 0, 3, -10, 10)   # Camera 0: negative X direction
-BBOX_1 = (-10, 10, 0, 3, -10, -0.1)   # Camera 1: negative Z direction
-BBOX_2 = (0.1, 10, 0, 3, -10, 10)     # Camera 2: positive X direction
-BBOX_3 = (-10, 10, 0, 3, 0.1, 10)     # Camera 3: positive Z direction
+BBOX_0 = (-10, 0, 0, 3, -10, 10)   # Camera 0: negative X direction
+BBOX_1 = (-10, 10, 0, 3, -10, 0)   # Camera 1: negative Z direction
+BBOX_2 = (0, 10, 0, 3, -10, 10)     # Camera 2: positive X direction
+BBOX_3 = (-10, 10, 0, 3, 0, 10)     # Camera 3: positive Z direction
 
 def main():
     parser = argparse.ArgumentParser(sys.argv[0], description="Create a PLY point cloud file that can be used to test registration algorithms")
     parser.add_argument("--distance", action="store", type=float, default="0.01", metavar="DIST", help="Camera 2 and 4 point clouds will be move DIST meters out from where they should be")
     parser.add_argument("--npoint", action="store", type=int, default=200000, metavar="COUNT", help="Create original point cloud of approximately this many points")
+    parser.add_argument("--input", action="store", metavar="PLYFILE", help="Use PLY file as input in stead of synthetic pointcloud. Npoint is ignored")
     parser.add_argument("--two", action="store_true", help="Output only camera 1 and 2 points (in stead of all 4)")
     parser.add_argument("--single", action="store_true", help="Modify only camera 2 (in stead of 2 and 4)")
     parser.add_argument("output", action="store", help="Output ply file")
@@ -27,13 +28,18 @@ def main():
     offsets = [(0,0,0), (0,0,-distance), (0,0,0), (0,0,distance)]
     if args.single:
         offsets[3] = (0, 0, 0)
-    pc = create_regtest_pointcloud(npoint, offsets, args.two)
+    if args.input:
+        orig_pc = cwipc.cwipc_read(args.input, 0)
+    else:
+        gen = cwipc.cwipc_synthetic(npoints=npoint)
+        orig_pc = gen.get()
+        gen.free()
+        gen = None
+    assert orig_pc
+    pc = create_regtest_pointcloud(orig_pc, offsets, args.two)
     cwipc.cwipc_write(output, pc)
 
-def create_regtest_pointcloud(npoint : int, offsets : List[MyPoint], twocam : bool=False) -> cwipc.cwipc_wrapper:
-    gen = cwipc.cwipc_synthetic(npoints=npoint)
-    pc = gen.get()
-    assert pc
+def create_regtest_pointcloud(pc : cwipc.cwipc_wrapper, offsets : List[MyPoint], twocam : bool=False) -> cwipc.cwipc_wrapper:
     pc0 = construct_partial_pc(pc, BBOX_0, offsets[0], 1)
     pc1 = construct_partial_pc(pc, BBOX_1, offsets[1], 2)
     pc2 = construct_partial_pc(pc, BBOX_2, offsets[2], 4)
