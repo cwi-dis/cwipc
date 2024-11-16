1d018fc8eb95e41d40be35d4d85f6a2100e76376
Jack Jansen <Jack.Jansen@cwi.nl>
2024-11-16 01:01:43 +0100

    Fixes to Python build/install process. Related to #159.

--- CMakeFiles/CwipcPythonSupport.cmake
+++ CMakeFiles/CwipcPythonSupport.cmake
@@ -77,8 +77,10 @@ macro(cwipc_build_wheel)
 	# We need a trick to pass CWIPC_VERSION in the environment at build time: use cmake -E env.
 	add_custom_target("${MYARGS_NAME}_wheel" 
 		ALL
+		COMMAND ${CMAKE_COMMAND} -E rm -rf ${MYARGS_SOURCEDIR}/build ${MYARGS_SOURCEDIR}/{${MYARGS_NAME}.egg_info 
 		COMMAND ${CMAKE_COMMAND} -E env "CWIPC_VERSION=${CWIPC_VERSION}" ${Python3_EXECUTABLE} -m build --wheel --no-isolation --outdir ${MYARGS_WHEELDIR} ${MYARGS_SOURCEDIR} 
 		COMMAND ${Python3_EXECUTABLE} -m pip uninstall -qq -y "${MYARGS_NAME}"
+		COMMAND ${Python3_EXECUTABLE} -m pip install -e ${MYARGS_SOURCEDIR}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 		)
 endmacro()
