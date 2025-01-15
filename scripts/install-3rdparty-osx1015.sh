db5470852737c597c95b95eb8afedd4f3e2c68c9
Jack Jansen <Jack.Jansen@cwi.nl>
2025-01-16 00:02:12 +0100

    Brew doesn't like pip-based installs anymore. Let's see if we can do without them.

--- scripts/install-3rdparty-osx1015.sh
+++ scripts/install-3rdparty-osx1015.sh
@@ -9,9 +9,3 @@ brew install homebrew/core/glfw3 || brew upgrade homebrew/core/glfw3
 brew install librealsense || brew upgrade librealsense
 brew install jpeg-turbo || brew upgrade jpeg-turbo
 brew link --force jpeg-turbo
-
-# Install Python delendencies
-python3.12 -m pip install --upgrade pip setuptools build wheel
-
-# Install Python dependencies for Deployment scripts (unsure they are still needed)
-python3.12 -m pip install requests requests_toolbelt mechanize pillow numpy open3d
