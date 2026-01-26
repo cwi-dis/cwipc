1a38ba7eab19a1a45c149e69dad5b5de98119240
Jack Jansen <Jack.Jansen@cwi.nl>
2026-01-26 15:18:39 +0100

    Orbbec needs openCV on mac. Added it.

--- scripts/install-3rdparty-osx1015.sh
+++ scripts/install-3rdparty-osx1015.sh
@@ -8,5 +8,6 @@ brew link python@3.12
 brew install pkg-config || brew upgrade pkg-config
 brew install homebrew/core/glfw3 || brew upgrade homebrew/core/glfw3
 brew install librealsense || brew upgrade librealsense
+brew install opencv || brew upgrade opencv
 brew install jpeg-turbo || brew upgrade jpeg-turbo
 brew link --force jpeg-turbo
