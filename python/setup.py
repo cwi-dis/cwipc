511cd439b2c1b9b3015d26cf1b49740e7d19ede8
Jack Jansen <Jack.Jansen@cwi.nl>
2026-03-08 22:21:53 +0100

    Updated for v8.0.0 release.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 def get_version():
-    CWIPC_VERSION="7.7+unknown"
+    CWIPC_VERSION="8.0+unknown"
     if 'CWIPC_VERSION' in os.environ:
         CWIPC_VERSION=os.environ['CWIPC_VERSION']
     return CWIPC_VERSION
